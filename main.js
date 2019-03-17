45c6a3d4f69daa30414ee4429295dee4c069780e
dylandaviddavies <dylandaviddavies@gmail.com>
2019-03-16 20:49:16 -0400

    Fixed JS and CSS weirdness
    Fixed weird JavaScript issues and removed unnecessary margins.
    
    Removed lambda functions.

--- main.js
+++ main.js
@@ -1,64 +1,96 @@
-function Rubber(v) {
+function rubberify(v) {
   let txt = v.innerText;
   v.setAttribute("aria-label", txt);
   v.innerHTML = "";
-  txt.split("").forEach(c => {
+  txt.split("").forEach(function(c) {
     if (c === "" || c === " ") {
       v.appendChild(document.createTextNode(c));
       return;
     }
     if (c === ",") {
-      v.appendChild(new Letter(c).element);
+      v.appendChild(readyLetter(c));
       v.appendChild(document.createElement("br"));
       return;
     }
-    v.appendChild(new Letter(c).element);
+    v.appendChild(readyLetter(c));
   });
 }
 
-function Letter(l) {
-  this.element = document.createElement("span");
-  this.element.style.display = "inline-block";
-  this.element.appendChild(document.createTextNode(l));
-  this.element.addEventListener("mouseover", event => {
+function readyLetter(l) {
+  console.log("creating letter: " + l);
+  let element = document.createElement("span");
+  element.style.display = "inline-block";
+  element.appendChild(document.createTextNode(l));
+  element.addEventListener("mouseover", function(event){
     event.target.classList.add("animated");
     event.target.classList.add("rubberBand");
   });
-  this.element.addEventListener("animationend", event => {
+  element.addEventListener("animationend", function(event) {
     event.target.classList.remove("animated");
     event.target.classList.remove("rubberBand");
   });
+  return element;
 }
 
-document.querySelectorAll(".rubbery").forEach(v => {
-  new Rubber(v);
+document.querySelectorAll(".rubbery").forEach(function(v) {
+  rubberify(v);
 });
 
 document.getElementById("contactBtn").addEventListener("click", event => {
-  flipOutFlipIn(document.getElementById("infoContainer"),document.getElementById("contactContainer"));
+  let contact = document.getElementById("contactContainer");
+  let info = document.getElementById("infoContainer");
+  if (anyAnimating([contact, info]))
+    return;
+  flipOutFlipIn(
+    info,
+    contact
+  );
 });
 
 document.getElementById("cancelContactBtn").addEventListener("click", event => {
-  flipOutFlipIn(document.getElementById("contactContainer"), document.getElementById("infoContainer"));
+  let contact = document.getElementById("contactContainer");
+  let info = document.getElementById("infoContainer");
+  if (anyAnimating([contact, info]))
+    return;
+  flipOutFlipIn(
+    contact,
+    info
+  );
 });
 
-function flipOutFlipIn(flipOut, flipIn){
+function anyAnimating(arr) {
+  for (let i = 0; i < arr.length; i++)
+    if (isAnimating(arr[i]))
+      return true;
+  return false;
+}
+
+function isAnimating(e) {
+  return e.classList.contains("animated");
+}
+
+function oneTimeEvent(element, eventType, callback) {
+  element.addEventListener(eventType, function (e) {
+    e.target.removeEventListener(e.type, arguments.callee);
+    return callback(e);
+  });
+}
+
+function flipOutFlipIn(flipOut, flipIn) {
+  console.log("flip out");
   flipOut.classList.add("animated");
   flipOut.classList.add("flipOutX");
-  var fadeIn = event => {
-  flipOut.classList.remove("animated");
-  flipOut.classList.remove("flipOutX");
- event.target.style.display = 'none';
+  oneTimeEvent(flipOut, "animationend", function () {
+    flipOut.style.display = 'none';
+    flipOut.classList.remove("animated");
+    flipOut.classList.remove("flipOutX");
+    console.log("flip in");
     flipIn.classList.add("animated");
-    flipIn.classList.add("flipInX"); 
-  flipIn.style.display = 'block';
-    var fadeOut = event => {
-      event.target.classList.remove("animated");
-      event.target.classList.remove("flipInX");
-      event.target.removeEventListener("animationend",fadeOut);
-    };
-    flipIn.addEventListener("animationend", fadeOut);
-    event.target.removeEventListener("animationend",fadeIn);
-  };
-  flipOut.addEventListener("animationend", fadeIn);
+    flipIn.classList.add("flipInX");
+    flipIn.style.display = 'block';
+    oneTimeEvent(flipIn, "animationend", function () {
+      flipIn.classList.remove("animated");
+      flipIn.classList.remove("flipInX");
+    });
+  });
 }
